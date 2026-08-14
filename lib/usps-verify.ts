306b10b76a7b71b1d307ae276883006dcd3d3122
Tawanna Verock <tawanna@myrealtyline.com>
2026-08-13 20:54:06 -0500

    Revert "fix(usps): send client credentials in token request"

--- lib/usps-verify.ts
+++ lib/usps-verify.ts
@@ -55,8 +55,6 @@ async function getAccessToken(): Promise<string> {
     },
     body: new URLSearchParams({
       grant_type: 'client_credentials',
-      client_id: id,
-      client_secret: secret,
     }).toString(),
   });
 
